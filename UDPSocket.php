ce2413d2b3b65ff9e63dcb5631a1c556fe30d751
dev-ys-36 <98698629+dev-ys-36@users.noreply.github.com>
2022-03-14 00:08:13 +0900

    cooooooooooooooooooooomplete

--- UDPSocket.php
+++ UDPSocket.php
@@ -1,29 +1,46 @@
 <?php
 
+# version: 1.0.0 (2022.3.14)
+
 $socket_list = [];
 
-function createSocket($port): bool {
+function createSocket(string $name, string $address, int $port): bool{
+
+    global $socket_list;
 
     if (isset($socket_list[$port])){
         return false;
     }
 
-    $socket = @socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-    $bind = socket_bind($socket, "0.0.0.0", $port);
+    $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+    if ($socket === false){
+        return false;
+    }
+
+    $bind = socket_bind($socket, $address, $port);
 
     if ($bind === false){
         return false;
     }
 
-    @socket_set_nonblock($socket);
+    $nonblock = socket_set_nonblock($socket);
 
-    $socket_list[$port] = $socket;
+    if ($nonblock === false){
+        return false;
+    }
+
+    $socket_list[$port] = [];
+    $socket_list[$port]['name'] = $name;
+    $socket_list[$port]['socket'] = $socket;
 
     return true;
 
 }
 
-function closeSocket($port): bool {
+function closeSocket(int $port): bool{
+
+    global $socket_list;
 
     if (!isset($socket_list[$port])){
         return false;
@@ -31,18 +48,66 @@ function closeSocket($port): bool {
 
     socket_close($socket_list[$port]);
 
+    unset($socket_list[$port]);
+
     return true;
     
 }
 
-function send(){
+function sendData(string $address, int $sendPort, int $recvPort, string $text): bool{
+
+    global $socket_list;
+
+    if (!isset($socket_list[$sendPort])){
+        return false;
+    }
+
+    $send = socket_sendto($socket_list[$sendPort]['socket'], $text, strlen($text), 0, $address, $recvPort);
+
+    if (!$send){
+        return false;
+    }
+
+    return true;
 
 }
 
-function receive(){
+function receiveData(?string &$address, ?int &$sendPort, int $recvPort, ?string &$text): bool{
+
+    global $socket_list;
+
+    if (!isset($socket_list[$recvPort])){
+        return false;
+    }
+
+    $recv = socket_recvfrom($socket_list[$recvPort]['socket'], $text, 1024, 0, $address, $sendPort);
+
+    if (!$recv){
+        return false;
+    }
+
+    return true;
 
 }
 
-createSocket('test', 1);
+
+$_1 = createSocket('test1', '127.0.0.1', 1);
+$_2 = createSocket('test2', '127.0.0.1', 2);
+
+if (!$_1 or !$_2){
+    return;
+}
+
+while(true){
+
+    sendData('127.0.0.1', 1, 2, 'test message');
+
+    receiveData($address, $sendPort, 2, $text);
+
+    var_dump($text);
+    
+    sleep(1);
+
+}
 
 ?>
